e5bf52626c8afb7ec9e59a5598a30b453e47c402
Vinu Suhas <vinusuhas2000@gmail.com>
2023-11-10 21:40:41 +0530

    added some on homepage

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ console.log('Bye world');
 const https=require('http')
 const jsonData=require('./DataStore')
 const server=https.createServer((request,response)=>{
+   let nodeAdvantages= [
+        "Efficient asynchronous operations",
+        "Scalability and high performance",
+        "Rich ecosystem of modules via npm",
+        "Real-time applications support",
+        "Fast code execution with V8 engine",
+        "Cross-platform compatibility",
+        "Handling of concurrent requests efficiently",
+        "Simplified development with JavaScript",
+        "Streamlined event-driven architecture"
+      ]
+      nodeAdvantages= nodeAdvantages.map(element=>`<li>${element}</li>`)
     if(request.url==="/"){
-        response.write('<h1>This is Homepage</h1>')
-
+        response.write('<h1>What is nodejs</h1><p>Node.js is a cross-platform, open-source server environment that can run on Windows, Linux, Unix, macOS, and more. Node.js is a back-end JavaScript runtime environment, runs on the V8 JavaScript engine, and executes JavaScript code outside a web browser</p>')
+        response.write('Advantages of Nodejs')
+        response.write(`<ul>${nodeAdvantages}</ul>`)
     }
     else if(request.url==='/product'){
 
